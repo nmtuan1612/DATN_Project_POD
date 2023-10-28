2026a0b8b747bc0c43453dd43f713f5caffee0ac
NMTuan <47388278+nmtuan1612@users.noreply.github.com>
2023-10-28 20:48:22 +0700

    fix api key

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,7 +7,8 @@ dotenv.config()
 const router = express.Router()
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+  // apiKey: process.env.OPENAI_API_KEY
+  apiKey: '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'
 })
 
 const openai = new OpenAIApi(config)
